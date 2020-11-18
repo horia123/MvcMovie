0edff07750d55fc6e84e77ecb0c1e5cc9b05e13d
Horia123 <hori.neamtu@gmail.com>
2020-11-18 09:07:28 +0200

    Create DbContext class
    Dbcontext class needed to coordinate EF Core functinoality ( CRUD ) for Movie model. the DbContext is derived from Microsoft.EFCore.DbContext.
    Creates a DbSet<Movie> property for the entity set. in EF terminology, an entity set typically corresponds to a db table. An entity corresponds to a row in the table.

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public DbSet<Movie> Movie { get; set; }
+
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+
+        }
+    }
+}
